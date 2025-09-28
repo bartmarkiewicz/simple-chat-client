7defd40cc91b78b367703a399c463a39c57bff8f
bartmarkiewicz <bartmark322@gmail.com>
2025-09-28 18:39:57 +0100

    Fix static check errors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
@@ -15,7 +16,13 @@ func main() {
 	go clientManager.StartWebSocketServer()
 
 	http.HandleFunc("/ws", clientManager.WebsocketPage)
-	err := http.ListenAndServe(":8080", nil)
+
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 60 * time.Second,
+	}
+
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error starting web server:", err)
 		return
